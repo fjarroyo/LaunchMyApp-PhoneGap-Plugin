87e250ec29e37eb232ea60499b95b8daf0b71091
fjarroyo@autentia.com <fjarroyo@autentia.com>
2014-04-11 12:59:28 +0200

    adding traces to launchapp.js

--- www/android/LaunchMyApp.js
+++ www/android/LaunchMyApp.js
@@ -1,6 +1,8 @@
 (function () {
     "use strict";
-    
+
+    console.log('declarando triggerOpenUrl');
+
     function triggerOpenURL() {
           cordova.exec(
               (typeof handleOpenURL == "function" ? handleOpenURL : null),
@@ -9,7 +11,10 @@
               "checkIntent",
               []);
     }
-    
+
+    console.log('DeviceReady');
     document.addEventListener("deviceready", triggerOpenURL, false);
+    console.log('resume');
     document.addEventListener("resume", triggerOpenURL, false);
+    console.log('done');
 }());
