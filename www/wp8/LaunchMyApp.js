05db55ee86c460767cdcb57398736ff89a0265d7
fjarroyo@autentia.com <fjarroyo@autentia.com>
2014-04-14 11:52:56 +0200

    Removed unnecesary method

--- www/wp8/LaunchMyApp.js
+++ www/wp8/LaunchMyApp.js
@@ -10,15 +10,6 @@
             []);
     }
 
-    function getUriData(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            "LaunchMyApp",
-            "getUriData",
-            []);
-    }
-
     document.addEventListener("deviceready", triggerOpenURL, false);
     document.addEventListener("resume", triggerOpenURL, false);
 }());
