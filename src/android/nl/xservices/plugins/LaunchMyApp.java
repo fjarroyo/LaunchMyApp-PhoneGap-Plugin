4f5fa72d05bed34c3202910d143b11d3dd295e10
fjarroyo@autentia.com <fjarroyo@autentia.com>
2014-04-21 16:29:07 +0200

    Add getUriData action to native code

--- src/android/nl/xservices/plugins/LaunchMyApp.java
+++ src/android/nl/xservices/plugins/LaunchMyApp.java
@@ -12,20 +12,29 @@ public class LaunchMyApp extends CordovaPlugin {
 
   private static final String ACTION_CHECKINTENT = "checkIntent";
 
+  private static final String ACTION_GETURIDATA = "getUriData";
+
+  private String uriData = "";
+
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
     if (ACTION_CHECKINTENT.equalsIgnoreCase(action)) {
-      final Intent intent = ((CordovaActivity) this.webView.getContext()).getIntent();
-      if (intent.getDataString() != null) {
-        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, intent.getDataString()));
+        final Intent intent = ((CordovaActivity) this.webView.getContext()).getIntent();
+        if (intent.getDataString() != null) {
+            uriData = intent.getDataString();
+            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, uriData));
+            intent.setData(null);
+            return true;
+        } else {
+            callbackContext.error("App was not started via the launchmyapp URL scheme. Ignoring this errorcallback is the best approach.");
+            return false;
+        }
+    } else if (ACTION_GETURIDATA.equalsIgnoreCase(action)){
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, uriData));
         intent.setData(null);
         return true;
-      } else {
-        callbackContext.error("App was not started via the launchmyapp URL scheme. Ignoring this errorcallback is the best approach.");
-        return false;
-      }
     } else {
-      callbackContext.error("This plugin only responds to the " + ACTION_CHECKINTENT + " action.");
+      callbackContext.error("This plugin only responds to the " + ACTION_CHECKINTENT + ", " + ACTION_GETURIDATA + " action.");
       return false;
     }
   }
